a2c8e4fe8648ae943e18f05e6191d8563263fdc9
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-22 13:36:15 -0500

    hover only for special item

--- assets/js/app.js
+++ assets/js/app.js
@@ -104,15 +104,19 @@ window.stencilBootstrap = function stencilBootstrap(
 
 $('.card-figure').hover(
   function () {
-    var image = $(this).find('.card-image');
-    var newImg = image.attr('data-hoverimage');
-    var currentImg = image.attr('src');
-    if (newImg && newImg != '') image.attr('src', newImg);
+    if (this.parentElement.dataset.test == 'card-112') {
+      var image = $(this).find('.card-image');
+      var newImg = image.attr('data-hoverimage');
+      var currentImg = image.attr('src');
+      if (newImg && newImg != '') image.attr('src', newImg);
+    }
   },
   function () {
-    var image = $(this).find('.card-image');
-    var newImg = image.attr('data-src');
-    var currentImg = image.attr('src');
-    if (newImg && newImg != '') image.attr('src', newImg);
+    if (this.parentElement.dataset.test == 'card-112') {
+      var image = $(this).find('.card-image');
+      var newImg = image.attr('data-src');
+      var currentImg = image.attr('src');
+      if (newImg && newImg != '') image.attr('src', newImg);
+    }
   }
 );

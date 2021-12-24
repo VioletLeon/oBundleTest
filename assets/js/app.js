d11d1a4379423f5dabc70501cd742059c9446030
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-23 19:39:37 -0500

    fixed adding all to cart button

--- assets/js/app.js
+++ assets/js/app.js
@@ -101,22 +101,3 @@ window.stencilBootstrap = function stencilBootstrap(
     },
   };
 };
-
-$('.card-figure').hover(
-  function () {
-    if (this.parentElement.dataset.test == 'card-112') {
-      var image = $(this).find('.card-image');
-      var newImg = image.attr('data-hoverimage');
-      var currentImg = image.attr('src');
-      if (newImg && newImg != '') image.attr('src', newImg);
-    }
-  },
-  function () {
-    if (this.parentElement.dataset.test == 'card-112') {
-      var image = $(this).find('.card-image');
-      var newImg = image.attr('data-src');
-      var currentImg = image.attr('src');
-      if (newImg && newImg != '') image.attr('src', newImg);
-    }
-  }
-);
